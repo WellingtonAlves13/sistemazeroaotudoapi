813b96b4a491470a5a41d4e4e95ad008d571d021
WellingtonAlves13 <wellington2009alves@gmail.com>
2024-01-17 18:10:35 -0300

    usuarioDTO ajust

--- src/main/java/br/com/blue/projeto/DTO/UsuarioDTO.java
+++ src/main/java/br/com/blue/projeto/DTO/UsuarioDTO.java
@@ -1,10 +1,6 @@
 package br.com.blue.projeto.DTO;
 
 import br.com.blue.projeto.entity.UsuarioEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import org.springframework.beans.BeanUtils;
 
 public class UsuarioDTO {
